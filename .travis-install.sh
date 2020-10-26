9c9d427946c9f1a1085b402712f12e96a0ff8243
odidev <odidev@puresoftware.com>
2020-10-26 16:18:35 +0530

    Update .travis-install.sh

--- .travis-install.sh
+++ .travis-install.sh
@@ -3,14 +3,14 @@
 # mysql: makefs
 sudo apt-get update
 sudo apt-get install makefs
-sudo apt-get install openjdk-7-jdk
+sudo apt-get install openjdk-8-jdk
 
 # ovs: build process requires 'six' module
 pip install six
 
 # Build and install rumprun toolchain from source
 RUMPRUN_PLATFORM=${RUMPRUN_PLATFORM:-hw}
-RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE:-x86_64-rumprun-netbsd}
+RUMPRUN_TOOLCHAIN_TUPLE=${RUMPRUN_TOOLCHAIN_TUPLE:-aarch64-rumprun-netbsd}
 
 git clone -q https://github.com/rumpkernel/rumprun /tmp/rumprun
 (
